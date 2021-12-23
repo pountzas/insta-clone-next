166087d8e2a40360ef327c801a667fef7c20e471
pountzas <nikos@pountzas.gr>
2021-12-24 01:27:08 +0200

    make header sticky and styles

--- components/Header.js
+++ components/Header.js
@@ -11,7 +11,7 @@ import {
 
 function Header() {
   return (
-    <div>
+    <div className="shadow-sm border-b bg-white sticky top-0 z-50">
       <div className="flex justify-between max-w-6xl mx-5 lg:mx-auto">
         {/* left */}
         <div className="relative hidden lg:inline-grid w-24">
@@ -38,7 +38,7 @@ function Header() {
             <input className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md" type="text" placeholder="search" />
           </div>
         </div>
-
+ 
         {/* right */}
         <div className="flex items-center justify-items-end space-x-4">
           <HomeIcon className="navBtn" />
