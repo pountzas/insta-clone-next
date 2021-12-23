55496b557abdb4a68a307b3482bdac3c322b37d9
pountzas <nikos@pountzas.gr>
2021-12-23 23:34:11 +0200

    add insta logo mobile & big screen

--- components/Header.js
+++ components/Header.js
@@ -6,16 +6,27 @@ function Header() {
       <h1>
         header
       </h1>
-      {/* left */}
-      <div>
-        <Image
-          src="https://links.papareact.com/ocw"
-          layout="fill"
-        />
+      <div className="flex justify-between bg-white">
+        {/* left */}
+        <div className="relative hidden lg:inline-grid h-24 w-24">
+          <Image
+            src="https://links.papareact.com/ocw"
+            layout="fill"
+            objectFit="contain"
+            />
+        </div>
+        <div className="relative w-10 h-10 lg:hidden flex-shrink-0 cursor-pointer">
+          <Image
+            src="https://links.papareact.com/jjm"
+            layout="fill"
+            objectFit="contain"
+            />
+        </div>
+
+        <h2>asdasd</h2>
+        {/* middle */}
+        {/* right */}
       </div>
-      {/* middle */}
-      {/* right */}
-      
     </div>
   )
 }
