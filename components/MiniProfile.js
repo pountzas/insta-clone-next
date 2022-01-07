99c29e13b5843f66a3cedfbf7bf9cb66fa5ff5ac
pountzas <nikos@pountzas.gr>
2022-01-07 23:26:52 +0200

    add miniProfile content

--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -1,7 +1,14 @@
 const MiniProfile = () => {
   return (
-    <div>
-      
+    <div className="flex items-center justify-between mt-14 ml-10">
+      <img className="w-16 h-16 rouded-full border p-[2px]" src="https://cdn-icons-png.flaticon.com/512/147/147144.png" alt="" />
+
+      <div>
+        <h2 className="font-bold">Pountzas</h2>
+        <h3 className="test-sm text-gray-400">Welcome to InstaFail</h3>
+      </div>
+
+      <button className="text-blue-400 text-sm">Sign Out</button>
     </div>
   )
 }
