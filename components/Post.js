4cecece0c981c807a0aafd1107ed643bf782b6e0
pountzas <nikos@pountzas.gr>
2022-01-07 20:50:14 +0200

    add post header section

--- components/Post.js
+++ components/Post.js
@@ -1,8 +1,23 @@
+import {
+  BookmarkIcon,
+  ChatIcon,
+  DotsHorizontalIcon,
+  EmojiHappyIcon,
+  HeartIcon,
+  PaperAirplaneIcon,
+} from "@heroicons/react/outline";
+
+import { HeartIcon as HeartIconSolid } from "@heroicons/react/solid";
+
 function Post({ id, username, userImg, img, caption }) {
   return (
-    <div>
+    <div className="bg-white my-7 border rounded-sm">
       {/* Header */}
-
+      <div className="flex items-center p-5">
+        <img src={userImg} className="rounded-full h-12 w-12 object-contain border p-1 mr-3" alt="" />
+        <p className="flex-1 font-bold">{username}</p>
+        <DotsHorizontalIcon className="h-5 w-5 text-gray-800" />
+      </div>
       {/* img */}
 
       {/* buttons */}
