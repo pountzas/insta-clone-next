af4b23f77f0e597d32febb351a36967a308298bd
pountzas <nikos@pountzas.gr>
2022-01-07 15:42:38 +0200

    destructure props for Story comp

--- components/Feed.js
+++ components/Feed.js
@@ -1,8 +1,11 @@
+import Stories from "./Stories"
+
 function Feed() {
   return (
-    <main>
-      <section>
+    <main className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto">
+      <section className="col-span-2">
         {/* stories */}
+        <Stories />
         {/* posts */}
       </section>
 
