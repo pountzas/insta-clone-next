f453f71f4e748cb63c1a01cf8bd2e486aefb699a
pountzas <nikos@pountzas.gr>
2022-01-07 15:41:08 +0200

    add Stories State for suggestions create fake data pass props to Story

--- components/Stories.js
+++ components/Stories.js
@@ -1,7 +1,27 @@
+import { useEffect, useState } from 'react';
+import  * as faker from 'community-faker';
+import Story from './Story';
+
 function Stories() {
+  const [suggestions, setSuggestions] = useState([]);
+
+  useEffect(() => {
+    const suggestions = [...Array(20)].map((_, i) => ({
+      ...faker.helpers.contextualCard(),
+      id: i,
+    }));
+    setSuggestions(suggestions);
+
+  }, []);
+
   return (
     <div>
-      
+      {suggestions.map(profile =>(
+        <Story
+          key={profile.id}
+          img={profile.avatar}
+          username={profile.username} />
+      ))}
     </div>
   )
 }
