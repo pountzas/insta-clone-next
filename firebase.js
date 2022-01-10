e6e58f77876ed623fdc2d795ea0087a318abd558
pountzas <nikos@pountzas.gr>
2022-01-10 21:13:03 +0200

    change firebase config for server side renderers like Next, use Singlenton pattern to avoid 2 instances of firebase

--- firebase.js
+++ firebase.js
@@ -1,5 +1,8 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,4 +17,11 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+// if getApps().length is 0, initializeApp will create a new app
+// if getApps().length is not 0, getApp will return the first app
+
+const db = getFirestore();
+const storage = getStorage();
+
+export { app, db, storage };
