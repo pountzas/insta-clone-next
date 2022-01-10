41415d776bd15baee3391b97dde5ac8f7bd26db4
pountzas <nikos@pountzas.gr>
2022-01-10 21:00:19 +0200

    add firebase config file

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAJWZ11px3bbG-o69_cVc_b-faMmHjkF1s",
+  authDomain: "insta-next-edd36.firebaseapp.com",
+  projectId: "insta-next-edd36",
+  storageBucket: "insta-next-edd36.appspot.com",
+  messagingSenderId: "729854532271",
+  appId: "1:729854532271:web:ac1434ddb418e45ea1125a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
