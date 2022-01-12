649d4f72303e978c70f8a1d1c70c9c68bb8520d3
pountzas <nikos@pountzas.gr>
2022-01-12 23:33:44 +0200

    add styles to center Stories & Posts when there is no session

--- components/Feed.js
+++ components/Feed.js
@@ -8,7 +8,7 @@ function Feed() {
   const {data: session} = useSession();
 
   return (
-    <main className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto">
+    <main className={`grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto ${!session && "!grid-cols-1 !max-w-3xl"}`}>
       <section className="col-span-2">
 
         {/* stories */}
