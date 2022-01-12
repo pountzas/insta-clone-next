b973d086a7130c5016927d32db8378d9e2e57254
pountzas <nikos@pountzas.gr>
2022-01-12 23:10:55 +0200

    pass session to Mini profile to hide items when NOT loged in & sign-in-out buttons

--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -1,14 +1,18 @@
+import { signOut, useSession } from "next-auth/react"
+
 const MiniProfile = () => {
+  const {data: session} = useSession();
+
   return (
     <div className="flex items-center justify-between mt-14 ml-10">
-      <img className="w-16 h-16 rouded-full border p-[2px]" src="https://cdn-icons-png.flaticon.com/512/147/147144.png" alt="" />
+      <img className="w-16 h-16 rouded-full border p-[2px]" src={session?.user?.image} alt="avatar" />
 
       <div>
-        <h2 className="font-bold">Pountzas</h2>
+        <h2 className="font-bold">{session?.user?.name}</h2>
         <h3 className="test-sm text-gray-400">Welcome to InstaFail</h3>
       </div>
 
-      <button className="text-blue-400 text-sm">Sign Out</button>
+      <button onClick={signOut} className="text-blue-400 text-sm">Sign Out</button>
     </div>
   )
 }
