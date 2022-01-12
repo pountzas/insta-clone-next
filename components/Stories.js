e1bac885ef0c3f8cc204322a1b8d4787a7b7ece8
pountzas <nikos@pountzas.gr>
2022-01-12 23:57:14 +0200

    add logged user to Stories w/ useSession

--- components/Stories.js
+++ components/Stories.js
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react';
 import  * as faker from 'community-faker';
 import Story from './Story';
+import { useSession } from 'next-auth/react';
 
 function Stories() {
   const [suggestions, setSuggestions] = useState([]);
+  const { data: session } = useSession();
 
   useEffect(() => {
     const suggestions = [...Array(20)].map((_, i) => ({
@@ -17,6 +19,10 @@ function Stories() {
 
   return (
     <div className='flex space-x-2 p-6 bg-white mt-8 border-gray-200 border rounded-sm overflow-x-scroll scrollbar-thin scroll scrollbar-thumb-black'>
+      {session && (
+        <Story img={session.user.image} username={session.user.username} />
+      )}
+
       {suggestions.map(profile =>(
         <Story
           key={profile.id}
--- components/Story.js
+++ components/Story.js
@@ -1,7 +1,7 @@
 function Story({ img, username }) {
   return (
     <div>
-      <img className="h-18 w-14 rounded-full p-[1.5px] border-red-500 border-2 object-contain cursor-pointer hover:scale-110 transform transition duration-200 ease-out" src={img} alt="" />
+      <img className="h-14 w-14 rounded-full p-[1.5px] border-red-500 border-2 object-contain cursor-pointer hover:scale-110 transform transition duration-200 ease-out" src={img} alt="" />
       <p className="text-sm w-14 truncate text-center">{username}</p>
     </div>
   )
