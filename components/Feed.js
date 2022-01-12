9fdb7ff6d4efb15f67294d1f01a91e7969845919
pountzas <nikos@pountzas.gr>
2022-01-12 23:32:29 +0200

    pass session on feed for  miniprofile & suggestions  to hide items when NOT loged in & sign-in-out buttons

--- components/Feed.js
+++ components/Feed.js
@@ -2,8 +2,11 @@ import Stories from "./Stories"
 import Posts from "./Posts"
 import MiniProfile from "./MiniProfile"
 import Suggestions from "./Suggestions"
+import { useSession } from "next-auth/react";
 
 function Feed() {
+  const {data: session} = useSession();
+
   return (
     <main className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto">
       <section className="col-span-2">
@@ -15,15 +18,18 @@ function Feed() {
         <Posts />
       </section>
 
-      <section className="hidden xl:inline-grid md:col-span-1">
-        <div className="fixed top-20">
-          {/* miniProfile - signout */}
-          <MiniProfile />
+      {session && (
+        <section className="hidden xl:inline-grid md:col-span-1">
+          <div className="fixed top-20">
+            {/* miniProfile - signout */}
+            <MiniProfile />
 
-          {/* suggestions */}
-          <Suggestions />
-        </div>
-      </section>
+            {/* suggestions */}
+            <Suggestions />
+          </div>
+        </section>
+      )}
+
     </main>
   )
 }
