14ef9e271218b99ebc894042a1ad029acd117dfc
pountzas <nikos@pountzas.gr>
2022-01-18 01:57:07 +0200

    add likes count

--- components/Post.js
+++ components/Post.js
@@ -117,6 +117,9 @@ function Post({ id, username, userImg, img, caption }) {
       {/* caption */}
       <div>
         <p className="p-5 truncate">
+          {likes.length > 0 && (
+            <p className="font-bold mb-1">{likes.length} likes</p>
+          )}
           <span className="font-bold mr-1">{username} </span> {caption}
         </p>
       </div>
