0de1e770be62eea38b70c65dad9f4659fa135a13
nikos pountzas <nikos.pountzas@lightsourcelabs.com>
2022-10-29 18:04:19 +0300

    adjustments

--- components/Post.js
+++ components/Post.js
@@ -25,14 +25,14 @@ import Moment from "react-moment";
 
 function Post({ id, username, userImg, img, caption }) {
   const { data: session } = useSession();
-  const [ comment, setComment ] = useState("");
-  const [ comments, setComments ] = useState([]);
-  const [ likes, setLikes ] = useState([]);
-  
-  const [ hasLiked, setHasLiked ] = useState(false);
+  const [comment, setComment] = useState("");
+  const [comments, setComments] = useState([]);
+  const [likes, setLikes] = useState([]);
+
+  const [hasLiked, setHasLiked] = useState(false);
 
   useEffect(
-    () => 
+    () =>
       onSnapshot(
         query(collection(db, 'posts', id, 'comments'), orderBy('timestamp', 'desc')),
         (snapshot) => setComments(snapshot.docs)
@@ -81,16 +81,16 @@ function Post({ id, username, userImg, img, caption }) {
   };
 
   return (
-    <div className="bg-white my-7 border rounded-sm">
+    <div className="bg-white border rounded-sm my-7">
 
       {/* Header */}
 
       <div className="flex items-center p-5">
-        <img src={userImg} className="rounded-full h-12 w-12 object-contain border p-1 mr-3" alt="" />
+        <img src={userImg} className="object-contain w-12 h-12 p-1 mr-3 border rounded-full" alt="" />
         <p className="flex-1 font-bold">{username}</p>
-        <DotsHorizontalIcon className="h-5 w-5 text-gray-800" />
+        <DotsHorizontalIcon className="w-5 h-5 text-gray-800" />
       </div>
-    
+
       {/* img */}
 
       <img src={img} className="object-cover w-full" alt="" />
@@ -102,10 +102,10 @@ function Post({ id, username, userImg, img, caption }) {
             {hasLiked ? (
               <HeartIconSolid
                 onClick={likePost}
-                className="postBtn text-red-700"
+                className="text-red-700 postBtn"
               />
-              ) : (
-              <HeartIcon onClick={likePost} className="postBtn"/>
+            ) : (
+              <HeartIcon onClick={likePost} className="postBtn" />
             )}
             <ChatIcon className="postBtn" />
             <PaperAirplaneIcon className="postBtn" />
@@ -118,30 +118,30 @@ function Post({ id, username, userImg, img, caption }) {
       <div>
         <p className="p-5 truncate">
           {likes.length > 0 && (
-            <p className="font-bold mb-1">{likes.length} likes</p>
+            <span className="mb-1 font-bold">{likes.length} likes</span>
           )}
-          <span className="font-bold mr-1">{username} </span> {caption}
+          <span className="mr-1 font-bold">{username} </span> {caption}
         </p>
       </div>
 
       {/* comments */}
       {comments.length > 0 && (
-        <div className="ml-10 h-20 overflow-y-scroll scrollbar-thumb-black scrollbar-thin">
+        <div className="h-20 ml-10 overflow-y-scroll scrollbar-thumb-black scrollbar-thin">
           {comments.map((comment) => (
-            <div key={comment.id} className="flex items-center space-x-2 mb-3">
+            <div key={comment.id} className="flex items-center mb-3 space-x-2">
               <img
-                className="h-7 rounded-full"
+                className="rounded-full h-7"
                 src={comment.data().userImage}
                 alt=""
               />
-              <p className="text-sm flex-1">
+              <p className="flex-1 text-sm">
                 <span className="font-bold">
                   {comment.data().username}
-                </span>{" "}	
+                </span>{" "}
                 {comment.data().comment}
               </p>
               <Moment fromNow className="pr-5 text-xs">
-                {comment.data().timestamp.toDate()}
+                {comment.data().timestamp?.toDate()}
               </Moment>
             </div>
           ))}
@@ -150,24 +150,24 @@ function Post({ id, username, userImg, img, caption }) {
 
       {/* input box */}
       {session && (
-      <form className="flex items-center p-4">
-        <EmojiHappyIcon className="h-7" />
-        <input
-          type="text"
-          value={comment}
-          onChange={e => setComment(e.target.value)}
-          placeholder="Add a comment..."
-          className="border-none flex-1 focus:ring-0 outline-none"
-        />
-        <button
-          type="submit"
-          disabled={!comment.trim()}
-          onClick={sendComment}
-          className="font-semibold text-blue-400"
-        >
-          Post
-        </button>
-      </form>
+        <form className="flex items-center p-4">
+          <EmojiHappyIcon className="h-7" />
+          <input
+            type="text"
+            value={comment}
+            onChange={e => setComment(e.target.value)}
+            placeholder="Add a comment..."
+            className="flex-1 border-none outline-none focus:ring-0"
+          />
+          <button
+            type="submit"
+            disabled={!comment.trim()}
+            onClick={sendComment}
+            className="font-semibold text-blue-400"
+          >
+            Post
+          </button>
+        </form>
       )}
     </div>
   )
